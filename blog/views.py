58325f99f4979ee4869bab1ab069cc3f78729a90
mali44 <maalogy@gmail.com>
2017-04-23 17:54:27 +0300

    Veritabani postlari erisim

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
+def post_list (request):
+    posts= Post.objects.filter(yayinlanma_tarihi__lte=timezone.now()) 
+    return render(request,'blog/post_list.html',{'posts':posts})
