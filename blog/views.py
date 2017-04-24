964d0f595c84a876615b847d524162f17b9c7438
mali44 <maalogy@gmail.com>
2017-04-24 04:40:25 +0300

    Blog'a css ve template eklendi

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
+
 # Create your views here.
 def post_list (request):
-    posts= Post.objects.filter(yayinlanma_tarihi__lte=timezone.now()) 
+    posts= Post.objects.filter(yayinlanma_tarihi__lte=timezone.now())
+
     return render(request,'blog/post_list.html',{'posts':posts})
+
+
+def post_detail(request,pk):
+    post= get_object_or_404(Post , pk=pk)
+    #return render(request,'blog/post_list.html',{'posts':post7
+    return render (request,'blog/post_detail.html',{'post':post})
